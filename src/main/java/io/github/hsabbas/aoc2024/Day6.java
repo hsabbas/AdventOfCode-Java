ea558bf2128dccdd053dfcce09b999f947533921
Hassan <habbas375@gmail.com>
2024-12-06 22:08:04 -0600

    Remembered records exist

--- src/main/java/io/github/hsabbas/aoc2024/Day6.java
+++ src/main/java/io/github/hsabbas/aoc2024/Day6.java
@@ -3,9 +3,9 @@ package io.github.hsabbas.aoc2024;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
 
 public class Day6 {
+    record Position(int x, int y){}
     public static void main(String[] args) throws IOException{
         solve();
     }
@@ -17,55 +17,30 @@ public class Day6 {
         }
 
         char direction = 'U';
-        Position guard = new Position();
+        Position guard = null;
         for(int row = 0; row < grid.length; row++) {
             for(int col = 0; col < grid[row].length; col++) {
                 if(grid[row][col] == '^') {
-                    guard.y = row;
-                    guard.x = col;
+                    guard = new Position(col, row);
                     grid[row][col] = direction;
                 }
             }
         }
+        if(guard == null) {
+            System.out.println("Could not find guard");
+            System.exit(0);
+        }
+
+        Position start = new Position(guard.x, guard.y);
         
         int partOne = 1;
         int partTwo = 0;
-
-        Map<Position, Set<Character>> visited = new HashMap<>();
 
         while (inBounds(grid, guard)) {
             if(grid[guard.y][guard.x] == '.'){
                 partOne++;
                 grid[guard.y][guard.x] = direction;
             }
-
-            char loopCheckDir = getRightTurn(direction);
-            Position loopCheckPos = getNextPosition(guard, loopCheckDir);
-            while (inBounds(grid, loopCheckPos) && grid[loopCheckPos.y][loopCheckPos.x] != '#'){
-                boolean working = true;
-                for(Position test : visited.keySet()){
-                    if(loopCheckPos.equals(test)){
-                        //How could this be false?
-                        working = visited.containsKey(loopCheckPos);
-                        break;
-                    }
-                }
-                if(!working){
-                    System.out.println(working);
-                }
-                if(visited.containsKey(loopCheckPos) && visited.get(loopCheckPos).contains(loopCheckDir)){
-                    System.out.println("Test");
-                    partTwo++;
-                    break;
-                }
-                loopCheckPos = getNextPosition(loopCheckPos, loopCheckDir);
-            }
-
-
-            if(!visited.containsKey(guard)){
-                visited.put(guard, new HashSet<>());
-            }
-            visited.get(guard).add(direction);
 
             Position next = getNextPosition(guard, direction);
             if(!inBounds(grid, next)){
@@ -78,6 +53,7 @@ public class Day6 {
 
             guard = next;
         }
+
         System.out.println(partOne);
         System.out.println(partTwo);
     }
@@ -102,28 +78,5 @@ public class Day6 {
 
     private static boolean inBounds(char[][] grid, Position guard) {
         return guard.y >= 0 && guard.y < grid.length && guard.x >= 0 && guard.x < grid[guard.y].length;
-    }
-}
-
-class Position{
-    public int x = 0, y = 0;
-
-    public Position() {}
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if(obj.getClass() != Position.class){
-            return false;
-        }
-        return equals((Position) obj);
-    }
-
-    public boolean equals(Position other) {
-        return x == other.x && y == other.y;
     }
 }
