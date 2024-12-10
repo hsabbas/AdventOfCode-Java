784187443178dc1f60ee0d04f0b0c193b1c163d5
Hassan <habbas375@gmail.com>
2024-12-10 00:04:25 -0600

    Added solution for day 10 part 2.

--- src/main/java/io/github/hsabbas/aoc2024/Day10.java
+++ src/main/java/io/github/hsabbas/aoc2024/Day10.java
@@ -13,6 +13,12 @@ public class Day10 {
         try (BufferedReader reader = new BufferedReader(new FileReader("C:/AdventOfCode2024/Day10/input.txt"))){
             grid = reader.lines().map(String::toCharArray).toArray(char[][]::new);
         }
+
+        solve1(grid);
+        solve2(grid);
+    }
+
+    private static void solve1(char[][] grid) {
         int totalScore = 0;
         for(int row = 0; row < grid.length; row++) {
             for(int col = 0; col < grid[row].length; col++) {
@@ -59,6 +65,62 @@ public class Day10 {
             score += scoreTrailHead(grid, nextPosition, (char) (curr + 1), visited);
         }
         return score;
+    }
+
+    private static void solve2(char[][] grid) {
+        int totalRating = 0;
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[row].length; col++) {
+                if(grid[row][col] == '0') {
+                    totalRating += rateTrailHead(grid, new Coords(col, row), '0', new HashSet<>());
+                }
+            }
+        }
+        System.out.println(totalRating);
+    }
+
+    private static int rateTrailHead(char[][] grid, Coords currPos, char curr, HashSet<Coords> currPath) {
+        if(curr == '9') {
+            return 1;
+        }
+
+        int rating = 0;
+        Coords nextPosition = new Coords(currPos.x + 1, currPos.y);
+        if(inBounds(grid, nextPosition)
+                && !currPath.contains(nextPosition)
+                && grid[nextPosition.y][nextPosition.x] == curr + 1){
+            currPath.add(nextPosition);
+            rating += rateTrailHead(grid, nextPosition, (char) (curr + 1), currPath);
+            currPath.remove(nextPosition);
+        }
+
+        nextPosition = new Coords(currPos.x, currPos.y + 1);
+        if(inBounds(grid, nextPosition)
+                && !currPath.contains(nextPosition)
+                && grid[nextPosition.y][nextPosition.x] == curr + 1){
+            currPath.add(nextPosition);
+            rating += rateTrailHead(grid, nextPosition, (char) (curr + 1), currPath);
+            currPath.remove(nextPosition);
+        }
+
+        nextPosition = new Coords(currPos.x - 1, currPos.y);
+        if(inBounds(grid, nextPosition)
+                && !currPath.contains(nextPosition)
+                && grid[nextPosition.y][nextPosition.x] == curr + 1){
+            currPath.add(nextPosition);
+            rating += rateTrailHead(grid, nextPosition, (char) (curr + 1), currPath);
+            currPath.remove(nextPosition);
+        }
+
+        nextPosition = new Coords(currPos.x, currPos.y - 1);
+        if(inBounds(grid, nextPosition)
+                && !currPath.contains(nextPosition)
+                && grid[nextPosition.y][nextPosition.x] == curr + 1){
+            currPath.add(nextPosition);
+            rating += rateTrailHead(grid, nextPosition, (char) (curr + 1), currPath);
+            currPath.remove(nextPosition);
+        }
+        return rating;
     }
 
     private static boolean inBounds(char[][] grid, Coords position) {
