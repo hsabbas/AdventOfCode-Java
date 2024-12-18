058423c35ca06a924aa8ad9a88ba72fdce3c0410
Hassan <habbas375@gmail.com>
2024-12-17 23:26:02 -0600

    Added solution for Day 16 part two.

--- src/main/java/io/github/hsabbas/aoc2024/Day16.java
+++ src/main/java/io/github/hsabbas/aoc2024/Day16.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 import java.util.*;
 
 public class Day16 {
-    static int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static int[][] directions = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     record Coords(int x, int y) {}
+    record Step(Coords coords, int direction) {}
     public static void main(String[] args) throws IOException {
         char[][] grid;
         try (BufferedReader reader = new BufferedReader(new FileReader("C:/AdventOfCode2024/Day16/input.txt"))){
@@ -30,19 +31,48 @@ public class Day16 {
         }
         assert reindeer != null;
 
-        System.out.println(travelMaze(grid, reindeer, 0, new HashSet<>(List.of(reindeer)), 0, new HashMap<>()));
+        List<Set<Coords>> bestPaths = new ArrayList<>();
+        int bestScore =
+                travelMaze(grid, reindeer, 0,
+                        new HashSet<>(List.of(reindeer)),
+                        0, Integer.MAX_VALUE,
+                        new HashMap<>(), bestPaths);
+
+
+        Set<Coords> bestSpots = new HashSet<>();
+        for(Set<Coords> path : bestPaths) {
+            bestSpots.addAll(path);
+        }
+
+        System.out.println(bestScore);
+        System.out.println(bestSpots.size());
     }
 
-    private static int travelMaze(char[][] grid, Coords reindeer, int currDirection, Set<Coords> currPath, int sum, Map<Coords, Integer> memo) {
+    private static int travelMaze(char[][] grid,
+                                  Coords reindeer,
+                                  int currDirection,
+                                  Set<Coords> currPath,
+                                  int sum,
+                                  int best,
+                                  Map<Step, Integer> memo,
+                                  List<Set<Coords>> bestPaths) {
         if(grid[reindeer.y][reindeer.x] == 'E') {
+            if(sum == best) {
+                Set<Coords> bestPath = new HashSet<>(currPath);
+                bestPaths.add(bestPath);
+            } else if (sum < best) {
+                Set<Coords> bestPath = new HashSet<>(currPath);
+                bestPaths.clear();
+                bestPaths.add(bestPath);
+            }
             return sum;
         }
 
-        if(memo.containsKey(reindeer) && memo.get(reindeer) < sum) {
+        if(sum >= best) {
             return Integer.MAX_VALUE;
         }
 
-        memo.put(reindeer, sum);
+        memo.put(new Step(reindeer, currDirection), sum);
 
         int min = Integer.MAX_VALUE;
         for(int i = 0; i < directions.length; i++) {
@@ -50,20 +80,27 @@ public class Day16 {
             if(currPath.contains(next) || grid[next.y][next.x] == '#') {
                 continue;
             }
-            currPath.add(next);
 
             int points;
+            Step coordsDir = new Step(next, i);
             if(i == currDirection){
-                points = travelMaze(grid, next, i, currPath, sum + 1, memo);
+                if(memo.containsKey(coordsDir) && memo.get(coordsDir) < sum + 1) {
+                    continue;
+                }
+                currPath.add(next);
+                points = travelMaze(grid, next, i, currPath, sum + 1, best, memo, bestPaths);
             } else {
-                points = travelMaze(grid, next, i, currPath, sum + 1001, memo);
+                if(memo.containsKey(coordsDir) && memo.get(coordsDir) < sum + 1001) {
+                    continue;
+                }
+                currPath.add(next);
+                points = travelMaze(grid, next, i, currPath, sum + 1001, best, memo, bestPaths);
             }
 
-            if(points == Integer.MAX_VALUE) {
-                currPath.remove(next);
-                continue;
+            if(points != Integer.MAX_VALUE) {
+                min = Math.min(min, points);
+                best = Math.min(best, min);
             }
-            min = Math.min(min, points);
 
             currPath.remove(next);
         }
